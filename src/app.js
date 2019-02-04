f4ceff903275a5b4006027a3bf21c7916661ab3b
0xKoddoelo <0xKoddoelo@gmail.com>
2019-02-04 23:37:22 +0700

    RxJS Quickstart

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
-// import { ajax } from 'rxjs/ajax';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, scan } from 'rxjs/operators';
 import { debounceTime, delay } from 'rxjs/operators';
 
 console.log('RxJS Boiler Running...');
@@ -10,7 +9,9 @@ const btn = $('#btn');
 const input = $('#input');
 const output = $('#output');
 
-const input$ = Rx.Observable.fromEvent(input, 'keyup').pipe(debounceTime(2000));
+const btn$ = Rx.Observable.fromEvent(btn, 'click').pipe(scan(x => x +1, 0));
+btn$.subscribe(x => document.getElementById('btnOut').innerText = 'You Clicked ' + x);
+const input$ = Rx.Observable.fromEvent(input, 'keyup').pipe(debounceTime(200));
 
 const findUserByName = (name) => {
   return $.ajax({
@@ -19,12 +20,22 @@ const findUserByName = (name) => {
   }).promise();
 };
 
-const profileDOM = ({ login, avatar_url}) => `<div><h1>${login}</h1><img src="${avatar_url}" /></div>`;
+const profileDOM = ({ login, avatar_url, followers, following}) => `<div><h3>${login}</h3><p>Followers: ${followers}</p><p>Followings: ${following}</p><img style="width: 100%;" src="${avatar_url}" /></div>`;
 
 input$.subscribe(e => {
   Rx.Observable.fromPromise(findUserByName(e.target.value)).subscribe(result => {
     console.log('result is', result);
-    document.getElementById('output').innerHTML = profileDOM(result.data); 
+    const child = result.data.message ? 'NotFound' : profileDOM(result.data);
+    document.getElementById('output').innerHTML = child; 
   })
   console.log('typed ', e.target.value);
+});
+
+const interval$ = Rx.Observable.interval(1000).take(20);
+interval$.subscribe(e => {
+  document.getElementById('outInterval').innerText = e;
+});
+const timer$ = Rx.Observable.timer(5000, 500).take(20);
+timer$.subscribe(e => {
+  document.getElementById('outTimer').innerText = e;
 });
